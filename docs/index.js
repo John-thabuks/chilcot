391846d60888072daa09d2a4da5874aab89f9417
John-thabuks <thabuksjohn@gmail.com>
2023-11-06 14:35:01 +0300

    Foundation for working with JS

---
+++ docs/index.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+
+    const baseURL = "https://my-json-server.typicode.com/John-thabuks/chilcot/db";
+    const shopItems = document.getElementById("shop-items");
+    const cartIcon = document.querySelector("#cart-icon");
+    const cart = document.querySelector(".cart");
+    const closeCart = document.querySelector("#close-cart");
+    const cartContent = document.querySelector(".cart-content");
+
+    let cartItems = [];
+})
