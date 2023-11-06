c1ea7a38937193e9b51b00b974b6520106f7ad4d
John-thabuks <thabuksjohn@gmail.com>
2023-11-06 14:40:46 +0300

    Remove item from the cart

--- docs/index.js
+++ docs/index.js
@@ -51,6 +51,46 @@ document.addEventListener("DOMContentLoaded", ()=>{
         displayCartItems();
     }
 
-    
+    // Display items in the cart
+    function displayCartItems() {
+        cartContent.innerHTML = "";
+
+        cartItems.forEach((item) => {
+            const cartItem = document.createElement("div");
+            cartItem.classList.add("cart-box");
+            cartItem.innerHTML = `
+                <img src="${item.image}" alt="" class="cart-img">
+                <div class="detail-box">
+                    <div class="cart-product-title">${item.title}</div>
+                    <div class="cart-price">$${(item.price.replace('$', '') * item.quantity).toFixed(2)}</div>
+                    <input type="number" value="${item.quantity}" class="cart-quantity">
+                </div>
+                <i class="bx bxs-trash-alt cart-remove"></i>
+            `;
+
+            const removeButton = cartItem.querySelector(".cart-remove");
+            removeButton.addEventListener("click", () => {
+                removeFromCart(item);
+            });
+
+            const quantityInput = cartItem.querySelector(".cart-quantity");
+            quantityInput.addEventListener("change", () => {
+                updateQuantity(item, quantityInput.value);
+            });
+
+            cartContent.appendChild(cartItem);
+        });
+
+        updateTotal();
+    }
+
+    // Remove item from the cart
+    function removeFromCart(item) {
+        const index = cartItems.indexOf(item);
+        if (index > -1) {
+            cartItems.splice(index, 1);
+            displayCartItems();
+        }
+    }
 
 })
